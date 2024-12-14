39f3e734ffb05b357b94b12b8736e0126d8ecbb6
a.paziresh <a.paziresh@tirantsp.ir>
2024-12-14 14:10:10 +0330

    [ADD] dynamic route example

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,30 @@
-export default function Home() {
-  return <h1>پلتفرم تخصصی پزشکان</h1>
+type Post = {
+  id: number;
+  title: string;
+  body: string;
+};
+
+export default async function Home() {
+
+  const res = await fetch('https://jsonplaceholder.typicode.com/posts', {
+    cache: 'no-store'
+  }
+  )
+
+  const posts: Post[] =  await res.json()  
+
+  return (
+  <main>
+  <ul>
+    {
+    posts.map((post: Post) => (
+      <li key={post.id}>{post.title}</li>
+    ))
+    }
+
+  </ul>
+  <h1>پلتفرم تخصصی پزشکان</h1>
+  
+</main>
+);
 }
